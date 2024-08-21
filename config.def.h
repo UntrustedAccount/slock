3ea1f01654673cfce6c03c5da78f1666cce8dc04
Daniel Liu <daniel17liu@gmail.com>
2024-08-21 00:44:06 -0700

    logo + background custom patch

--- config.def.h
+++ config.def.h
@@ -1,9 +1,10 @@
 /* user and group to drop privileges to */
-static const char *user  = "daniel";
+static const char *user  = "nobody";
 static const char *group = "nobody";
 
 static const char *colorname[NUMCOLS] = {
-	[INIT] = "black",       /* after initialization */
+	[BACKGROUND] =   "black",     /* after initialization */
+	[INIT] =   "#2d2d2d",     /* after initialization */
 	[INPUT] = "#005577",    /* during input */
 	[FAILED] = "#CC3333",   /* wrong password */
 	[CAPS] = "red",         /* CapsLock on */
@@ -13,6 +14,7 @@ static const char *colorname[NUMCOLS] = {
  * Xresources preferences to load at startup
  */
 ResourcePref resources[] = {
+		{ "color8",       STRING,  &colorname[BACKGROUND] },
 		{ "color0",       STRING,  &colorname[INIT] },
 		{ "color4",       STRING,  &colorname[INPUT] },
 		{ "color1",       STRING,  &colorname[FAILED] },
@@ -23,4 +25,23 @@ ResourcePref resources[] = {
 static const int failonclear = 1;
 
 /* Background image path, should be available to the user above */
-static const char* background_image = "/home/daniel/.walls/nature/valley.png";
+static const char* background_image = "";
+
+/* insert grid pattern with scale 1:1, the size can be changed with logosize */
+static const int logosize = 75;
+static const int logow = 12;	/* grid width and height for right center alignment*/
+static const int logoh = 6;
+
+static XRectangle rectangles[9] = {
+	/* x	y	w	h */
+	{ 0,	3,	1,	3 },
+	{ 1,	3,	2,	1 },
+	{ 0,	5,	8,	1 },
+	{ 3,	0,	1,	5 },
+	{ 5,	3,	1,	2 },
+	{ 7,	3,	1,	2 },
+	{ 8,	3,	4,	1 },
+	{ 9,	4,	1,	2 },
+	{ 11,	4,	1,	2 },
+
+};
